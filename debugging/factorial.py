2efbe58134862ab5a656cffc1388de5f1c05d115
only1techgal <jenn.lipholo@gmail.com>
2024-09-02 12:31:29 +0200

    factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,17 +5,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-         # Comment out the decrement to cause an infinite loop
-         # n -= 1
-    
-    return result # Return result after the loop completes
+    return result
 
-if len(sys.argv) > 1:
-    try:
-        f = factorial(int(sys.argv[1]))
-        print(f)
-    except ValueError:
-        print("Please provide a valid integer as an argument.")
-else:
-    print("Usage: ./factorial.py <number>")
-
+f = factorial(int(sys.argv[1]))
+print(f)
