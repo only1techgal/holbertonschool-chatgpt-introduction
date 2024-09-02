ef9891117ba7d4f760299534c04b41aa81b32150
only1techgal <jenn.lipholo@gmail.com>
2024-09-02 13:53:42 +0200

    factorial.py

--- debugging/factorial_correction.py
+++ debugging/factorial_correction.py
@@ -2,22 +2,29 @@
 import sys
 
 def factorial(n):
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer")
     result = 1
     while n > 1:
         result *= n
-        n -= 1 # Decrement n
+        n -= 1
     return result
-    
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: ./factorial.py <number>")
         sys.exit(1)
+    
     try:
-        f = factorial(int(sys.argv[1]))
+        num = int(sys.argv[1])
+        if num < 0:
+            raise ValueError("Input must be a non-negative integer")
+        f = factorial(num)
         print(f)
-    except ValueError:
-        print("Invalid input. Please provide an integer.")
+    except ValueError as e:
+        print(e)
         sys.exit(1)
-        
+
+
 
 
